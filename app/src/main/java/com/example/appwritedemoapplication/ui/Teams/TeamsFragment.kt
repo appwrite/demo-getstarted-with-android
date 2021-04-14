13456e21b3242e751805236e4e79bf7bcfcf81c7
Christy Jacob <christyjacob4@gmail.com>
2021-04-15 02:53:05 +0530

    feat: added teams endpoints

--- app/src/main/java/com/example/appwritedemoapplication/ui/Teams/TeamsFragment.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Teams/TeamsFragment.kt
@@ -2,31 +2,69 @@ package com.example.appwritedemoapplication.ui.Teams
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.appwritedemoapplication.R
+import com.example.appwritedemoapplication.databinding.FragmentDatabaseBinding
+import com.example.appwritedemoapplication.databinding.FragmentTeamsBinding
+import com.example.appwritedemoapplication.ui.Database.DatabaseViewModel
 
 class TeamsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = TeamsFragment()
-    }
-
+    private lateinit var binding: FragmentTeamsBinding
     private lateinit var viewModel: TeamsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater ,
+            container: ViewGroup? ,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_teams, container, false)
-    }
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_teams,
+                container,
+                false
+        )
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding.createTeam.setOnClickListener{
+            viewModel.createTeam(binding.content.text)
+        }
+
+        binding.getTeams.setOnClickListener{
+            viewModel.getTeams()
+        }
+
+        binding.getTeam.setOnClickListener{
+            viewModel.getTeam(binding.teamId.text)
+        }
+
+        binding.deleteTeam.setOnClickListener{
+            viewModel.deleteTeam(binding.teamId.text)
+        }
+
+        binding.responseTV.movementMethod = ScrollingMovementMethod()
+
         viewModel = ViewModelProvider(this).get(TeamsViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.error.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
+                Toast.makeText(requireContext(), it.message , Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        viewModel.response.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let {
+                binding.responseTV.text = it
+            }
+        })
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/appwritedemoapplication/ui/Teams/TeamsViewModel.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Teams/TeamsViewModel.kt
@@ -1,7 +1,85 @@
 package com.example.appwritedemoapplication.ui.Teams
 
+import android.text.Editable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appwritedemoapplication.utils.Client
+import com.example.appwritedemoapplication.utils.Event
+import io.appwrite.exceptions.AppwriteException
+import io.appwrite.services.TeamsService
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class TeamsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _error = MutableLiveData<Event<AppwriteException>>().apply {
+        value = null
+    }
+    val error: LiveData<Event<AppwriteException>> = _error
+
+    private val _response = MutableLiveData<Event<String>>().apply {
+        value = null
+    }
+    val response: LiveData<Event<String>> = _response
+
+    private val teamsService by lazy {
+        TeamsService(Client.client)
+    }
+
+
+    fun createTeam(name: Editable?) {
+        viewModelScope.launch {
+            try {
+                var response = teamsService.create(name.toString())
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getTeams() {
+        viewModelScope.launch {
+            try {
+                var response = teamsService.list()
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getTeam(id: Editable?) {
+        viewModelScope.launch {
+            try {
+                var response = teamsService.get(id.toString())
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun deleteTeam(id: Editable?) {
+        viewModelScope.launch {
+            try {
+                var response = teamsService.delete(id.toString())
+                var json = response.body?.string()?.ifEmpty { "{}" }
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+
 }
