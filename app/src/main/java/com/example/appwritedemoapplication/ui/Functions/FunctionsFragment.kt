24c369343e003d8f5b725e725765b2df09184805
Christy Jacob <christyjacob4@gmail.com>
2021-04-15 10:28:22 +0530

    feat: make response text view selectable

--- app/src/main/java/com/example/appwritedemoapplication/ui/Functions/FunctionsFragment.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Functions/FunctionsFragment.kt
@@ -2,31 +2,66 @@ package com.example.appwritedemoapplication.ui.Functions
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.appwritedemoapplication.R
+import com.example.appwritedemoapplication.databinding.FragmentDatabaseBinding
+import com.example.appwritedemoapplication.databinding.FragmentFunctionsBinding
+import com.example.appwritedemoapplication.ui.Database.DatabaseViewModel
 
 class FunctionsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = FunctionsFragment()
-    }
-
+    private lateinit var binding: FragmentFunctionsBinding
     private lateinit var viewModel: FunctionsViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater ,
+            container: ViewGroup? ,
+            savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_functions, container, false)
-    }
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_functions,
+                container,
+                false
+        )
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        binding.createExecution.setOnClickListener{
+            viewModel.createExecution(binding.functionId.text)
+        }
+
+        binding.listExecutions.setOnClickListener{
+            viewModel.listExecutions(binding.functionId.text)
+        }
+
+        binding.getExecution.setOnClickListener{
+            viewModel.getExecution(binding.functionId.text, binding.executionId.text)
+        }
+
+
+        binding.responseTV.movementMethod = ScrollingMovementMethod()
+
         viewModel = ViewModelProvider(this).get(FunctionsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        viewModel.error.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
+                Toast.makeText(requireContext(), it.message , Toast.LENGTH_SHORT).show()
+            }
+        })
 
+        viewModel.response.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let {
+                binding.responseTV.setText(it)
+            }
+        })
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/appwritedemoapplication/ui/Functions/FunctionsViewModel.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Functions/FunctionsViewModel.kt
@@ -1,7 +1,72 @@
 package com.example.appwritedemoapplication.ui.Functions
 
+import android.text.Editable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appwritedemoapplication.utils.Client
+import com.example.appwritedemoapplication.utils.Event
+import io.appwrite.exceptions.AppwriteException
+import io.appwrite.services.FunctionsService
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class FunctionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _error = MutableLiveData<Event<AppwriteException>>().apply {
+        value = null
+    }
+    val error: LiveData<Event<AppwriteException>> = _error
+
+    private val _response = MutableLiveData<Event<String>>().apply {
+        value = null
+    }
+    val response: LiveData<Event<String>> = _response
+
+    private val functionsService by lazy {
+        FunctionsService(Client.client)
+    }
+
+
+    fun createExecution(id: Editable?) {
+        viewModelScope.launch {
+            try {
+                val response = functionsService.createExecution(id.toString())
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun listExecutions(id: Editable?) {
+        viewModelScope.launch {
+            try {
+                val response = functionsService.listExecutions(id.toString())
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getExecution(functionId: Editable? , executionId: Editable?) {
+        viewModelScope.launch {
+            try {
+                val response = functionsService.getExecution(functionId.toString(), executionId.toString())
+                var json = response.body?.string() ?: ""
+                json = JSONObject(json).toString(4)
+                _response.postValue(Event(json))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+
 }
