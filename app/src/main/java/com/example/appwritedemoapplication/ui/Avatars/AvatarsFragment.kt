bfbb04c293c2954029347b1084da4b60f2cf49ec
Christy Jacob <christyjacob4@gmail.com>
2021-04-16 10:09:49 +0530

    feat: added avatars

--- app/src/main/java/com/example/appwritedemoapplication/ui/Avatars/AvatarsFragment.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Avatars/AvatarsFragment.kt
@@ -5,27 +5,63 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.appwritedemoapplication.R
+import com.example.appwritedemoapplication.databinding.FragmentAvatarsBinding
+import com.example.appwritedemoapplication.databinding.FragmentDatabaseBinding
+import com.example.appwritedemoapplication.ui.Database.DatabaseViewModel
 
 class AvatarsFragment : Fragment() {
 
-    private lateinit var avatarsViewModel: AvatarsViewModel
+    private lateinit var binding: FragmentAvatarsBinding
+    private lateinit var viewModel: AvatarsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater ,
             container: ViewGroup? ,
             savedInstanceState: Bundle?
     ): View? {
-        avatarsViewModel =
-                ViewModelProvider(this).get(AvatarsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_avatars , container , false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        avatarsViewModel.text.observe(viewLifecycleOwner , Observer {
-            textView.text = it
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_avatars,
+                container,
+                false
+        )
+
+        binding.getBrowser.setOnClickListener{
+            viewModel.getBrowser(binding.code.text)
+        }
+
+        binding.getCountryCode.setOnClickListener{
+            viewModel.getCountryCode(binding.code.text)
+        }
+
+        binding.getCreditCard.setOnClickListener{
+            viewModel.getCreditCard(binding.code.text)
+        }
+
+        binding.getQrCode.setOnClickListener{
+            viewModel.getQrCode(binding.code.text)
+        }
+
+        viewModel = ViewModelProvider(this).get(AvatarsViewModel::class.java)
+        viewModel.error.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let { // Only proceed if the event has never been handled
+                Toast.makeText(requireContext(), it.message , Toast.LENGTH_SHORT).show()
+            }
         })
-        return root
+
+        viewModel.image.observe(viewLifecycleOwner, Observer { event ->
+            event?.getContentIfNotHandled()?.let {
+                binding.imageView.setImageBitmap(it)
+            }
+        })
+
+        return binding.root
     }
 }
--- app/src/main/java/com/example/appwritedemoapplication/ui/Avatars/AvatarsViewModel.kt
+++ app/src/main/java/com/example/appwritedemoapplication/ui/Avatars/AvatarsViewModel.kt
@@ -1,13 +1,80 @@
 package com.example.appwritedemoapplication.ui.Avatars
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.text.Editable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appwritedemoapplication.utils.Client
+import com.example.appwritedemoapplication.utils.Event
+import io.appwrite.exceptions.AppwriteException
+import io.appwrite.services.AvatarsService
+import io.appwrite.services.DatabaseService
+import kotlinx.coroutines.launch
 
 class AvatarsViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+    private val _error = MutableLiveData<Event<AppwriteException>>().apply {
+        value = null
     }
-    val text: LiveData<String> = _text
+    val error: LiveData<Event<AppwriteException>> = _error
+
+    private val _image = MutableLiveData<Event<Bitmap>>().apply {
+        value = null
+    }
+    val image: LiveData<Event<Bitmap>> = _image
+
+    private val avatarsService by lazy {
+        AvatarsService(Client.client)
+    }
+
+    fun getBrowser(code: Editable?) {
+        viewModelScope.launch {
+            try{
+                val response = avatarsService.getBrowser(code.toString())
+                val image = BitmapFactory.decodeStream(response.body!!.byteStream())
+                _image.postValue(Event(image))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getCountryCode(code: Editable?) {
+        viewModelScope.launch {
+            try{
+                val response = avatarsService.getFlag(code.toString())
+                val image = BitmapFactory.decodeStream(response.body!!.byteStream())
+                _image.postValue(Event(image))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getCreditCard(code: Editable?) {
+        viewModelScope.launch {
+            try{
+                val response = avatarsService.getCreditCard(code.toString())
+                val image = BitmapFactory.decodeStream(response.body!!.byteStream())
+                _image.postValue(Event(image))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
+    fun getQrCode(name: Editable?) {
+        viewModelScope.launch {
+            try{
+                val response = avatarsService.getQR(name.toString())
+                val image = BitmapFactory.decodeStream(response.body!!.byteStream())
+                _image.postValue(Event(image))
+            } catch (e: AppwriteException) {
+                _error.postValue(Event(e))
+            }
+        }
+    }
+
 }
